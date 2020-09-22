43f4007877b801fe9520e53359db1b5b87a56273
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2020-09-22 15:47:21 +0200

    Reorder scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 psql -h localhost -p 5434 -U sirene -d sirene -c "DROP TABLE IF EXISTS siren"
 psql -h localhost -p 5434 -U sirene -d sirene -c "DROP TABLE IF EXISTS siret"
-psql -h localhost -p 5434 -U sirene -d sirene -f "./create_siren.sql"
-psql -h localhost -p 5434 -U sirene -d sirene -f "./create_siret.sql"
+psql -h localhost -p 5434 -U sirene -d sirene -f "../sql/create_siren.sql"
+psql -h localhost -p 5434 -U sirene -d sirene -f "../sql/create_siret.sql"
 
