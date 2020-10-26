9f28ae690b52cb0a20d79eab856ed398458eee24
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2020-10-26 19:04:03 +0100

    Update correct column name

--- db/init/50-enrich-columns.sh
+++ db/init/50-enrich-columns.sh
@@ -1,6 +1,6 @@
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "ALTER TABLE siren ADD COLUMN tsv tsvector;"
 
-psql -U $POSTGRES_USER -d $POSTGRES_DB -c "UPDATE siren SET tsv = setweight(to_tsvector(coalesce(denominationunitelegale,'')), 'A') || setweight(to_tsvector(coalesce(sigle,'')), 'A') || setweight(to_tsvector(coalesce(nomunitelegale,'')), 'B') || setweight(to_tsvector(coalesce(prenom1unitelegale,'')), 'C');"
+psql -U $POSTGRES_USER -d $POSTGRES_DB -c "UPDATE siren SET tsv = setweight(to_tsvector(coalesce(denominationunitelegale,'')), 'A') || setweight(to_tsvector(coalesce(sigleunitelegale,'')), 'A') || setweight(to_tsvector(coalesce(nomunitelegale,'')), 'B') || setweight(to_tsvector(coalesce(prenom1unitelegale,'')), 'C');"
 
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE INDEX siren_tsv ON siren USING gin(tsv);"
 
