a75c2cee2e4d37541070b81555a55324ee359606
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2020-12-01 17:50:23 +0100

    Add function for etablissements route

--- db/init/60-create-view-and-function.sh
+++ db/init/60-create-view-and-function.sh
@@ -1,5 +1,48 @@
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "DROP VIEW IF EXISTS etablissements_view"
-psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE VIEW etablissements_view AS SELECT T.activiteprincipaleetablissement as activite_principale, N.activiteprincipaleunitelegale as activite_principale_entreprise, T.activiteprincipaleregistremetiersetablissement as activite_principale_registre_metier, N. categorieentreprise as categorie_entreprise, T.codecedexetablissement as cedex, T.codepostaletablissement as code_postal, T.datecreationetablissement as date_creation, N.datecreationunitelegale as date_creation_entreprise, T.datedebut as date_debut_activite, N.datederniertraitementunitelegale as date_mise_a_jour, T.enseigne1etablissement as enseigne, T.geo_adresse, T.geo_id, T.geo_l4, T.geo_l5, T.geo_ligne, T.geo_score, T.geo_type, T.etablissementsiege as is_siege, T.latitude, T.libellecommuneetablissement as libelle_commune, T.libellevoieetablissement as libelle_voie, T.longitude, N.categoriejuridiqueunitelegale as nature_juridique_entreprise, T.nic, N.nicsiegeunitelegale as nic_siege, N.nomunitelegale as nom, N.denominationunitelegale as nom_raison_sociale, T.numerovoieetablissement as numero_voie, N.prenom1unitelegale as prenom, N.sigleunitelegale as sigle, N.siren, T.siret, T.trancheeffectifsetablissement as tranche_effectif_salarie, N.trancheeffectifsunitelegale as tranche_effectif_salarie_entreprise, T.typevoieetablissement as type_voie, T.codecommuneetablissement as commune, N.tsv FROM siret T LEFT JOIN siren N ON N.siren = T.siren;"
+psql -U $POSTGRES_USER -d $POSTGRES_DB -c "
+CREATE VIEW etablissements_view AS 
+    SELECT 
+        T.activiteprincipaleetablissement as activite_principale, 
+        N.activiteprincipaleunitelegale as activite_principale_entreprise, 
+        T.activiteprincipaleregistremetiersetablissement as activite_principale_registre_metier, 
+        N. categorieentreprise as categorie_entreprise, 
+        T.codecedexetablissement as cedex, 
+        T.codepostaletablissement as code_postal, 
+        T.datecreationetablissement as date_creation, 
+        N.datecreationunitelegale as date_creation_entreprise, 
+        T.datedebut as date_debut_activite, 
+        N.datederniertraitementunitelegale as date_mise_a_jour, 
+        T.enseigne1etablissement as enseigne, 
+        T.geo_adresse, 
+        T.geo_id, 
+        T.geo_l4, 
+        T.geo_l5, 
+        T.geo_ligne, 
+        T.geo_score, 
+        T.geo_type, 
+        T.etablissementsiege as is_siege, 
+        T.latitude, 
+        T.libellecommuneetablissement as libelle_commune, 
+        T.libellevoieetablissement as libelle_voie, 
+        T.longitude, 
+        N.categoriejuridiqueunitelegale as nature_juridique_entreprise, 
+        T.nic, 
+        N.nicsiegeunitelegale as nic_siege, 
+        N.nomunitelegale as nom, 
+        N.denominationunitelegale as nom_raison_sociale, 
+        T.numerovoieetablissement as numero_voie, 
+        N.prenom1unitelegale as prenom, 
+        N.sigleunitelegale as sigle, 
+        N.siren, 
+        T.siret, 
+        T.trancheeffectifsetablissement as tranche_effectif_salarie, 
+        N.trancheeffectifsunitelegale as tranche_effectif_salarie_entreprise, 
+        T.typevoieetablissement as type_voie, 
+        T.codecommuneetablissement as commune, 
+        N.tsv 
+    FROM siret T 
+    LEFT JOIN siren N 
+    ON N.siren = T.siren;"
 
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "
 CREATE VIEW unitelegale_view AS 
@@ -92,7 +135,8 @@ CREATE VIEW etablissements_siren AS
         T.codecommuneetablissement as commune, 
         N.tsv,
         T.etatadministratifetablissement as etat_administratif_etablissement,
-        N.nombre_etablissements
+        N.nombre_etablissements,
+        N.nom_complet
     FROM siret T 
     LEFT JOIN siren N 
     ON N.siren = T.siren;"
@@ -328,6 +372,77 @@ BEGIN
                 ) t;    
     END IF;
 end;\$\$;"
+
+
+psql -U $POSTGRES_USER -d $POSTGRES_DB  -c "CREATE OR REPLACE FUNCTION get_etablissements (siren_search text) 
+returns table (
+    unite_legale jsonb
+) 
+language plpgsql
+as \$\$ 
+BEGIN
+    return query 
+        SELECT 
+                jsonb_agg(
+                    json_build_object(
+                        'activite_principale', t.activite_principale,
+                        'activite_principale_entreprise', t.activite_principale_entreprise,
+                        'activite_principale_registre_metier', t.activite_principale_registre_metier,
+                        'categorie_entreprise', t.categorie_entreprise,
+                        'cedex', t.cedex,
+                        'code_postal', t.code_postal,
+                        'date_creation', t.date_creation,
+                        'date_creation_entreprise', t.date_creation_entreprise,
+                        'date_debut_activite', t.date_debut_activite,
+                        'date_mise_a_jour', t.date_mise_a_jour,
+                        'enseigne', t.enseigne,
+                        'geo_adresse', t.geo_adresse,
+                        'geo_id', t.geo_id,
+                        'geo_l4', t.geo_l4,
+                        'geo_l5', t.geo_l5,
+                        'geo_ligne', t.geo_ligne,
+                        'geo_score', t.geo_score,
+                        'geo_type', t.geo_type,
+                        'is_siege', t.is_siege,
+                        'latitude', t.latitude,
+                        'libelle_commune', t.libelle_commune,
+                        'libelle_voie', t.libelle_voie,
+                        'longitude', t.longitude,
+                        'nature_juridique_entreprise', t.nature_juridique_entreprise,
+                        'nic', t.nic,
+                        'nic_siege', t.nic_siege,
+                        'nom', t.nom,
+                        'nom_raison_sociale', t.nom_raison_sociale,
+                        'numero_voie', t.numero_voie,
+                        'prenom', t.prenom,
+                        'sigle', t.sigle,
+                        'siren', t.siren,
+                        'siret', t.siret,
+                        'tranche_effectif_salarie', t.tranche_effectif_salarie,
+                        'tranche_effectif_salarie_entreprise', t.tranche_effectif_salarie_entreprise,
+                        'type_voie', t.type_voie,
+                        'commune', t.commune,
+                        'tsv', t.tsv,
+                        'nombre_etablissements', t.nombre_etablissements,
+                        'etat_administratif_etablissement', t.etat_administratif_etablissement,
+                        'nom_complet', t.nom_complet,
+                        'etablissements', t.etablissements_array,
+                        'etablissement_siege', t.etablissement_siege
+                    )
+                ) as unite_legale
+        FROM 
+            (
+                SELECT
+                    ul.*,
+                    (SELECT json_agg(t1) FROM (SELECT * from etablissements_siren WHERE siren = siren_search ORDER BY is_siege DESC,etat_administratif_etablissement) t1) as etablissements_array,
+                    (SELECT json_agg(t2) FROM (SELECT * from etablissements_siren WHERE siren = siren_search AND is_siege = 't') t2) as etablissement_siege
+                FROM 
+                    unitelegale_view ul
+                WHERE 
+                    ul.siren = siren_search
+            ) t;        
+end;\$\$;"
+
 
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "\copy (SELECT
     CASE WHEN nature_juridique_entreprise = '1000' THEN
