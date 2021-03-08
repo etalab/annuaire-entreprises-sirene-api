c266e7e2425ad2264b9c54678849eb39602ef16b
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2021-03-08 17:01:46 +0100

    Introduce stopwords in proxy

--- aio/aio-proxy/aio_proxy/routes.py
+++ aio/aio-proxy/aio_proxy/routes.py
@@ -3,21 +3,31 @@ from aiohttp import web
 
 ESCAPED_CHARS = "!@#$\'\"()/&`\\§°*£%+=_;.,?:<>"
 
+
+TYPEVOIES = ['promenade','avenue','boulevard','place', 'carrefour', 'passage', 'allee', 'impasse', 'lieu dit', 'hameau', 'rue']
+
 routes = web.RouteTableDef()
 
 
 def sanatize_param(param):
     param = param.translate({ord(c): None for c in ESCAPED_CHARS})
-    param = ' & '.join(param.split())
+    #param = ' & '.join(param.split())
     return param.lower()
 
+def remove_typvoies(param):
+    qw = param.split()
+    res = [word for word in qw if word.lower() not in TYPEVOIES]
+    return ' & '.join(res)
 
 @routes.get('/search')
 async def search_endpoint(request):
     output_url = request.app['config']['output_url_search']
 
+    searchRes = sanatize_param(request.rel_url.query['q'].replace("-"," "))
+    searchRes = remove_typvoies(searchRes)
+
     json_body = {
-        'search': sanatize_param(request.rel_url.query['q'].replace("-"," ")),
+        'search': searchRes,
         'page_ask': request.rel_url.query['page'],
         'per_page_ask': request.rel_url.query['per_page']
     }
