51aa0d9740cc43c0409d9ca054ae269ec8e4e47e
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2021-04-06 11:39:59 +0200

    Update siren api

--- aio/aio-proxy/aio_proxy/routes.py
+++ aio/aio-proxy/aio_proxy/routes.py
@@ -42,9 +42,14 @@ async def search_endpoint(request):
 @routes.get('/siren')
 async def search_endpoint(request):
     output_url = request.app['config']['output_url_siren']
+    try:
+        page_ask = request.rel_url.query['page']
+    except:
+        page_ask = 1
 
     json_body = {
-        'siren_search': request.rel_url.query['q']
+        'siren_search': request.rel_url.query['q'],
+        'page_ask': page_ask
     }
 
     async with request.app['http_session'].post(output_url, json=json_body) as resp:
